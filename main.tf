b202bdbc61ebb476f9f18a51d43b926ceb1b74b0
Ankita <ankitagomkar73@gmail.com>
2025-07-24 03:57:02 +0000

    VPC file

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 resource "aws_s3_bucket" "s3bucket" {
-  bucket = "ankita-myfile-bucket"
-}
-
-resource "aws_s3_bucket_acl" "s3bucket_acl" {
-  bucket = aws_s3_bucket.s3bucket.id
-  acl    = "private"
-}
-
+    bucket = "ankita-file-bucket"
+    acl    = "private"
  
+    tags = {
+        Name        = "bucketterra"
+        Environment = "Dev"
+    }
+}
